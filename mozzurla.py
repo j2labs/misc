dcc408023b2b649fd8bb9bc897fdc3e44b1f248b
James Dennis <jdennis@gmail.com>
2009-10-14 18:47:20 -0400

    Added mozzurla.py, a python class for opening urls with mozilla as the user agent.

---
+++ mozzurla.py
@@ -0,0 +1,4 @@
+from urllib import FancyURLopener
+
+class MozillaOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; sv-SE; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3'
