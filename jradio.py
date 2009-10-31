4c4b87be29c293e616735d9bf4303df9fa71656f
James Dennis <jdennis@gmail.com>
2009-10-31 15:58:05 -0400

    Added support for station descriptions printed according to screen width

--- jradio.py
+++ jradio.py
@@ -13,17 +13,18 @@ from BeautifulSoup import BeautifulSoup
 
 mp3_player = '/opt/local/bin/mpg321 -q '
 somafm_url = 'http://somafm.com/'
-stations_info = {
-    'Bassdrive': 'http://www.bassdrive.com/v2/streams/BassDrive.pls',
-}
+stations_info = (
+    ('Bassdrive', 'http://www.bassdrive.com/v2/streams/BassDrive.pls', 'Drum n Bass Radio',),
+)
 
 class InternetRadioStation:
     """
     A simple representation of an internet radio station
     """
-    def __init__(self, name=None, pls_url=None, url=None):
+    def __init__(self, name=None, pls_url=None, desc=None):
         self.name = name
         self.pls_url = pls_url
+        self.desc = desc
         
     def play(self):
         urls_available = self.urls_from_pls(self.pls_url)
@@ -47,28 +48,83 @@ class InternetRadioStation:
                 urls.append(url_line)
         return urls
 
+    def pad_description(self, width, padding):
+        wrapped_desc = wrap(self.desc, width)
+        lines = wrapped_desc.split('\n')
+        padfu = lambda s: ' ' * padding + s + '\n'        
+        new_desc = map(padfu, lines)
+        return ''.join(new_desc).rstrip()
+
     def __str__(self):
-        return "%s ::: %s" % (self.name, self.pls_url)
+        return "%s" % (self.name)
+
+def wrap(text, width):
+    """
+    A word-wrap function that preserves existing line breaks
+    and most spaces in the text. Expects that existing line
+    breaks are posix newlines (\n).
+    Found at :: http://code.activestate.com/recipes/148061/
+    """
+    return reduce(lambda line, word, width=width: '%s%s%s' %
+                  (line,
+                   ' \n'[(len(line)-line.rfind('\n')-1
+                         + len(word.split('\n',1)[0]
+                              ) >= width)],
+                   word),
+                  text.split(' ')
+                 )
+
+
+def ioctl_GWINSZ(fd):
+    """
+    Tabulation functions for discovering terminal width
+    """
+    try:
+        import fcntl, termios, struct, os
+        cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
+    except:
+        return None
+    return cr
+
+def term_width():
+    cr = ioctl_GWINSZ(0)
+    if not cr:
+        return 80
+    else:
+        return cr[1]
 
 def crank_the_radio():
     # Initialize list with soma.fm
     station_list = scrape_somafm_info(somafm_url)
     
     # include additional pls files
-    for name in stations_info:
-        pls_url = stations_info[name]
-        station_list.append(make_stations(name,pls_url))
+    for station in stations_info:
+        station_obj = make_station(station)
+        station_list.append(station_obj)
         
     print "Stations:"
     for key, station in enumerate(station_list):
-        print "  %2d. %s" % (key, station)
+        print "  %2d. %s :: %s\n%s" % (key,
+                                       station.name,
+                                       station.pls_url,
+                                       station.pad_description((term_width()-6), 6))
     choice = input("Choice ?: ")
     station = station_list[choice]
     return station.play()
 
-def make_stations(name, pls):
-    return InternetRadioStation(name, pls)
+def make_station(station_tuple):
+    name = station_tuple[0]
+    pls = station_tuple[1]
+    desc = station_tuple[2]
+    return InternetRadioStation(name, pls, desc)
 
+def format_station_description(description):
+    description = description.replace("\n", "")
+    description = description.replace("  ", " ")
+    description = description.replace("  ", " ")
+    description = description.strip()
+    return description
+    
 def scrape_somafm_info(url):
     station_list = []
     page = urllib2.urlopen(url)
@@ -76,11 +132,14 @@ def scrape_somafm_info(url):
     soup = BeautifulSoup(html)
     for station in soup.fetch('li'):
         station_name = station.h3.contents[0]
+        station_desc = ' '.join(station('p', {'class':'descr'})[0].contents)
+        station_desc = format_station_description(station_desc)
         station_url = station.a['href']
         pls_name_info = station_url.rsplit('/')
         pls_url = '%s%s%s' % (url, pls_name_info[2], '.pls')
         irs = InternetRadioStation(name=station_name,
-                                   pls_url=pls_url)
+                                   pls_url=pls_url,
+                                   desc=station_desc)
         station_list.append(irs)
     return station_list
         
