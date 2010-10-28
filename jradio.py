145d5028d5b75a0aa17520f294626a4a81b4db21
James Dennis <james@extension.fm>
2010-10-28 05:17:54 -0400

    Added dubstep.fm to jradio

--- jradio.py
+++ jradio.py
@@ -16,6 +16,7 @@ mp3_player = '/opt/local/bin/mpg123 -q'
 somafm_url = 'http://somafm.com/'
 stations_info = (
     ('Bassdrive', 'http://www.bassdrive.com/v2/streams/BassDrive.pls', 'Drum n Bass Radio',),
+    ('Dubstep.fm', 'http://www.dubstep.fm/listen.pls', 'Dub Step',),
 )
 
 class InternetRadioStation:
